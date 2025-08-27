7e0362ad1c74857395657b672b18565469539a52
se3rik <se3rik@mail.ru>
2025-08-27 23:04:24 +0300

    fix: adding key property for posts

--- src/App.jsx
+++ src/App.jsx
@@ -13,11 +13,13 @@ import JournalForm from './components/JournalForm/JournalForm';
 import CardButton from './components/CardButton/CardButton';
 
 const INITIAL_DATA = [{
+	id: 1,
 	title: 'Подготовка к обновлению курсов',
 	text: 'Думал, что очень много времени',
 	date: new Date()
 },
 {
+	id: 2,
 	title: 'Поход в горы',
 	text: 'Горные походы открывают удивительные природные ландшафт',
 	date: new Date()
@@ -28,10 +30,15 @@ function App() {
 
 	function addJournalItem(newItem) {
 		setJournalItems((prev) => [...prev, {
+			id: Math.max(...prev.map(i => i.id)) + 1,
 			title: newItem.title,
 			text: newItem.text,
 			date: new Date(newItem.date)
 		}]);
+	}
+
+	function sortJournalItems(a, b) {
+		return a.date < b.date ? 1 : -1;
 	}
 
 	return (
@@ -40,8 +47,8 @@ function App() {
 				<Header/>
 				<JournalAddButton/>
 				<JournalList>
-					{journalItems.map((el) => (
-						<CardButton key={el.title}>
+					{journalItems.sort(sortJournalItems).map((el) => (
+						<CardButton key={el.id}>
 							<JournalItem 
 								title={el.title}
 								text={el.text}
