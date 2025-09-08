ee11afae6609042163b74c374012a0fc6eb5322d
se3rik <se3rik@mail.ru>
2025-09-08 20:55:01 +0300

    feat: base inline validation

--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -1,21 +1,55 @@
+import { useState } from 'react';
+
 import './JournalForm.css';
 
 import Button from './../Button/Button';
 
 function JournalForm({addJournalItem}) {
+	const [formValidState, setFormValidState] = useState({
+		title: true,
+		post: true,
+		date: true
+	});
+
 	function onFormSubmit(event) {
 		event.preventDefault();
 		const formData = new FormData(event.target);
 		const formProps = Object.fromEntries(formData);
+		let isFormValid = true;
+
+		if(!formProps.title.trim().length) {
+			setFormValidState(state => ({...state, title: false}));
+			isFormValid = false;
+		} else {
+			setFormValidState(state => ({...state, title: true}));
+		}
+
+		if(!formProps.post.trim().length) {
+			setFormValidState(state => ({...state, post: false}));
+			isFormValid = false;
+		} else {
+			setFormValidState(state => ({...state, post: true}));
+		}
+
+		if(!formProps.date) {
+			setFormValidState(state => ({...state, date: false}));
+			isFormValid = false;
+		} else {
+			setFormValidState(state => ({...state, date: true}));
+		}
+
+		if (!isFormValid) {
+			return;
+		}
 		addJournalItem(formProps);
 	}
 	
 	return (
 		<form className='journal-from' onSubmit={onFormSubmit}>
-			<input type="text" name='title' />
-			<input type="date" name='date' />
+			<input type="text" name='title' style={{border: formValidState.title ? undefined : '1px solid red'}}/>
+			<input type="date" name='date' style={{border: formValidState.date ? undefined : '1px solid red'}}/>
 			<input type="text" name='tag' />
-			<textarea name="text" />
+			<textarea name="post" style={{border: formValidState.post ? undefined : '1px solid red'}}/>
 			<Button text="Сохранить"/>
 		</form>
 	);
